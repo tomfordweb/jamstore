8eb92d1b9fee19850ea998b604e0f7a7c2f3c179
Tom Ford <tomfordweb@gmail.com>
2020-12-13 14:39:29 -0500

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,10 +27,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
